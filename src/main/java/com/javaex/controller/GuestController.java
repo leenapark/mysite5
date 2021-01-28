faae16e5ff08f1f74f75fb28fbea0c944c546088
leenapark <hrn424@gmail.com>
2021-01-28 18:34:48 +0900

    20210128 controller

---
+++ src/main/java/com/javaex/controller/GuestController.java
@@ -0,0 +1,79 @@
+package com.javaex.controller;
+
+import java.security.Timestamp;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javaex.service.GuestService;
+import com.javaex.vo.GuestVo;
+
+@Controller
+@RequestMapping(value="/guest")
+public class GuestController {
+
+	//필드
+	@Autowired
+	private GuestService guestService;
+	
+	//생성자
+	//g/s
+	
+	// 리스트
+	@RequestMapping(value="/list", method = {RequestMethod.GET, RequestMethod.POST})
+	public String list(Model model) {
+		System.out.println("list");
+		
+		List<GuestVo> addList = guestService.getList();
+		
+		model.addAttribute("addList", addList);
+		
+		return "guestbook/addList";
+	}
+	
+	// 방명록 생성
+	@RequestMapping(value="/add", method = {RequestMethod.GET, RequestMethod.POST})
+	public String add(@ModelAttribute GuestVo guestVo) {
+		System.out.println("add");
+		System.out.println("guestController guestVo: " + guestVo.toString());
+		
+		int count = guestService.insert(guestVo);
+				
+		return "redirect:/guest/list";
+	}
+	
+	// 삭제 폼
+	@RequestMapping(value="/deleteForm", method = {RequestMethod.GET, RequestMethod.POST})
+	public String deleteForm() {
+		System.out.println("daleteForm");
+		
+		
+		return "guestbook/deleteForm";
+	}
+
+	// 삭제
+	@RequestMapping(value="/delete", method = {RequestMethod.GET, RequestMethod.POST})
+	public String delete(@ModelAttribute GuestVo guestVo) {
+		System.out.println("delete");
+		System.out.println("guestController deleteForm: " + guestVo.toString());
+				
+		int count = guestService.delete(guestVo);
+		
+		if(count==1) {
+			System.out.println("삭제 성공");
+			
+			return "redirect:/guest/list";
+		} else {
+			System.out.println("삭제 실패");
+			
+			return "redirect:/guest/deleteForm?result=fail&no="+guestVo.getNo();
+		}
+	}
+}
--- src/main/java/com/javaex/controller/UserController.java
+++ src/main/java/com/javaex/controller/UserController.java
@@ -4,19 +4,21 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.javaex.dao.UserDao;
+import com.javaex.service.UserService;
 import com.javaex.vo.UserVo;
 
 @Controller
 @RequestMapping(value="/user")
 public class UserController {
 	//*필드
+	
 	@Autowired
-	private UserDao userDao;
+	private UserService userService;
 	
 	//*생성자
 	//*메소드 g/s
@@ -38,7 +40,7 @@ public class UserController {
 		// 어트리뷰트 확인
 		System.out.println("controller userVo: " + userVo.toString());
 		
-		userDao.insert(userVo);
+		int count = userService.join(userVo);
 		
 		return "user/joinOk";
 	}
@@ -59,7 +61,8 @@ public class UserController {
 		// 어트리뷰트 확인
 		System.out.println("controller userVo: " + userVo.toString());
 		
-		UserVo authUser = userDao.selectUser(userVo);
+		UserVo authUser = userService.login(userVo);
+		
 		if(authUser == null) {
 			System.out.println("로그인 실패");
 			
@@ -74,5 +77,55 @@ public class UserController {
 		}
 		
 	}
+	
+	// 로그아웃
+	@RequestMapping(value="/logout", method = {RequestMethod.GET, RequestMethod.POST})
+	public String logout(HttpSession session) {
+		System.out.println("logout");
+		
+		session.removeAttribute("authUser");
+		session.invalidate();
+		
+		return "redirect:/";
+	}
+	
+	
+	// 회원정보 수정 폼
+	@RequestMapping(value="/mForm", method = {RequestMethod.GET, RequestMethod.POST})
+	public String mForm(HttpSession session, Model model) {
+		System.out.println("mForm");
+		
+		UserVo authUser = (UserVo) session.getAttribute("authUser");
+		
+		UserVo userVo = userService.mForm(authUser.getNo());
+		
+		// 정보 확인 겸 진행 확인
+		System.out.println("ahthUser: " + userVo);
+		
+		model.addAttribute("ahthUser", userVo);
+		
+		return "user/modifyForm";
+	}
+	
+	// 회원정보 수정
+	@RequestMapping(value="/modify", method = {RequestMethod.GET, RequestMethod.POST})
+	public String modify(@ModelAttribute UserVo userVo, HttpSession session) {
+		System.out.println("modify");
+		UserVo authUser = (UserVo) session.getAttribute("authUser");
 
+		int no = authUser.getNo();
+		
+		userVo.setNo(no);
+		
+		int count = userService.modify(userVo);
+		
+		authUser.setName(userVo.getName());
+		
+		
+		return "redirect:/";
+		
+		
+	}
+	
+	
 }
