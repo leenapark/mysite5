17dc40b2c37b5843e58e9abfcb39ecc9e446477c
leenapark <hrn424@gmail.com>
2021-02-03 01:03:55 +0900

    20210203 Dao

--- src/main/java/com/javaex/dao/RboardDao.java
+++ src/main/java/com/javaex/dao/RboardDao.java
@@ -1,6 +1,8 @@
 package com.javaex.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +20,15 @@ public class RboardDao {
 	
 	
 	// 게시글 전체 가져오기
-	public List<RboardVo> getList() {
+	public List<RboardVo> getList(String str) {
 		System.out.println("Dao getList");
 		
-		List<RboardVo> rbList = sqlSession.selectList("rboard.selectList");
+		Map<String, Object> boardMap = new HashMap<String, Object>();
+		boardMap.put("str", str);
+		
+		System.out.println("Dao getList: " + boardMap);
+		
+		List<RboardVo> rbList = sqlSession.selectList("rboard.selectList", boardMap);
 		
 		return rbList;
 	}
@@ -48,11 +55,29 @@ public class RboardDao {
 		return sqlSession.update("rboard.hitUp", no);
 	}
 	
-	// 답글 추가 필드 수정
-	public void reUpdate(int no) {
-		System.out.println("Dao hitUp: " + no);
-		sqlSession.update("rboard.reUpdate", no);
+	// 답글 등록(저장)
+	public int reInsert(RboardVo rboardVo) {
+		System.out.println("Dao reInsert: " + rboardVo.toString());
 		
+		// update로 먼저 등록되어 있는 글의 계층 필드 값을 수정해준다
+		sqlSession.update("rboard.reUpdate", rboardVo);
+		
+		return sqlSession.insert("rboard.reinsert", rboardVo);
 	}
+	
+	// 게시글 삭제
+	public int delete(int no) {
+		System.out.println("Dao delete");
+		
+		return sqlSession.delete("rboard.delete", no);
+	}
+	
+	// 게시글 수정
+	public int update(RboardVo rboardVo) {
+		System.out.println("Dao update: " + rboardVo);
+		
+		return sqlSession.update("rboard.update", rboardVo);
+	}
+	
 	
 }
