4ab0fb85ec03f2b7d54287a48041a0092dc7d64f
leenapark <hrn424@gmail.com>
2021-02-03 01:03:44 +0900

    20210203 controller

--- src/main/java/com/javaex/controller/Rboardcontroller.java
+++ src/main/java/com/javaex/controller/Rboardcontroller.java
@@ -28,10 +28,10 @@ public class Rboardcontroller {
 	
 	// 첫화면
 	@RequestMapping(value="/main", method= {RequestMethod.GET, RequestMethod.POST})
-	public String rboardlist(Model model) {
+	public String rboardlist(Model model, @RequestParam(value="str", required=false) String str) {
 		System.out.println("controller rList");
 		
-		List<RboardVo> rbList = rService.getList();
+		List<RboardVo> rbList = rService.getList(str);
 		System.out.println("rbList: " + rbList.toString());
 		
 		
@@ -66,6 +66,7 @@ public class Rboardcontroller {
 		
 		UserVo authUser = (UserVo) session.getAttribute("authUser");
 		RboardVo rboardVo = rService.postRead(authUser, no);
+		System.out.println("controller postRead: " + rboardVo.toString());
 		
 		session.setAttribute("rpostVo", rboardVo);
 		
@@ -77,8 +78,6 @@ public class Rboardcontroller {
 	public String rewriteForm() {
 		System.out.println("controller rewriteForm");
 		
-		
-		
 		return "/rboard/rewriteForm";
 	}
 	
@@ -87,19 +86,40 @@ public class Rboardcontroller {
 	public String reWrite(@ModelAttribute RboardVo rboardVo) {
 		System.out.println("controller rewrite: " + rboardVo.toString());
 		
-		rService.reWrite(rboardVo);
+		int count = rService.reWrite(rboardVo);
 		
-		return "";
+		return "redirect:/rboard/main";
 	}
 	
+	// 게시글 삭제
+	@RequestMapping(value="/delete", method= {RequestMethod.GET, RequestMethod.POST})
+	public String delete(@RequestParam("no") int no) {
+		System.out.println("controller delete: " + no);
+		
+		int count = rService.delete(no);
+		
+		return "redirect:/rboard/main";
+	}
 	
-/*	public String rewrite(@RequestParam("no") int groupNo, @RequestParam("userNo") int userNo, @RequestParam("orderNo") int orderNo, @RequestParam("depth") int depth) {
-		System.out.println("controller rewrite: " + groupNo + ", " + userNo + ", " + orderNo + ", " + depth);
+	// 게시글 수정 폼
+	@RequestMapping(value="/modifyForm", method= {RequestMethod.GET, RequestMethod.POST})
+	public String modifyForm(@RequestParam("no") int no, Model model) {
+		System.out.println("controller modifyForm: " + no);
 		
+		RboardVo rboardVo = rService.modifyForm(no);
+		model.addAttribute("rboardVo", rboardVo);
 		
-		
-		return "/rboard/rewriteForm";
-	}*/
+		return "/rboard/modifyForm";
+	}
 	
+	// 게시글 수정
+	@RequestMapping(value="/modify", method= {RequestMethod.GET, RequestMethod.POST})
+	public String modify(@ModelAttribute RboardVo rboardVo) {
+		System.out.println("controller modify: " + rboardVo.toString());
+		
+		int count = rService.modify(rboardVo);
+		
+		return "redirect:/rboard/post?no="+rboardVo.getNo();
+	}
 	
 }
